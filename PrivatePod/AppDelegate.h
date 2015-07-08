f3879b592a2a48f5ef58c8e369864b15755c10be
adi rome <adirome@seekingalpha.com>
2015-07-08 11:51:39 +0300

    initial commit

---
+++ PrivatePod/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PrivatePod
+//
+//  Created by Adi Rome on 7/8/15.
+//  Copyright (c) 2015 Adi Rome. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
